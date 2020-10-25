4f99c9878e6c13239938ac3364a5906ac0076c9c
HwiChanYu <dbgnlcks34@gmail.com>
2020-10-25 10:06:57 +0900

    [2020.10.25]

---
+++ 2020_10_25.js
@@ -0,0 +1,24 @@
+// 단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+
+// 재한사항
+// s는 길이가 1 이상, 100이하인 스트링입니다.
+// 입출력 예
+// s	return
+// abcde   c
+// qwer   we
+
+function solution(s) {
+    let answer = '';
+    let order = 0;
+    if (s.length % 2 === 0) {
+      order = s.length / 2;
+      answer = s[order - 1] + s[order];
+    }
+    if (s.length % 2 === 1) {
+      order = Math.floor(s.length / 2);
+      answer = s[order];
+    }
+    return answer;
+}
+
+solution('qwer')
